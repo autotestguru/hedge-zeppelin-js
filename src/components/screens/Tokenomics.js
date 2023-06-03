5dd67cfd7ba284139ca5762fddae62d19e20a9a4
Gurpreet Singh <gpreet123@gmail.com>
2023-06-03 15:38:01 -0400

    Changed to green hedgie. Change tokenomic card animation

--- src/components/screens/Tokenomics.js
+++ src/components/screens/Tokenomics.js
@@ -1,7 +1,65 @@
 import Card from "../common/card/Card";
 import "./styles/tokenomics.css";
+import { useState } from "react";
 
 const Tokenomics = () => {
+  const [distFlip, setDistFlip] = useState({
+    prelaunch: false,
+    supply: false,
+    liquidity: false,
+  });
+
+  const DetailItem = (rowName, rowValue) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          width: "100%",
+          marginLeft: "10px",
+          marginRight: "10px",
+        }}
+      >
+        <span style={{ fontSize: "12px", marginBottom: "4px" }}>{rowName}</span>
+        <span
+          style={{ fontSize: "12px", marginBottom: "4px", textAlign: "right" }}
+        >
+          {rowValue.map((line) => (
+            <>
+              {line}
+              <br />
+            </>
+          ))}
+        </span>
+      </div>
+    );
+  };
+
+  const flip = (target) => {
+    let object;
+    switch (target) {
+      case "prelaunch":
+        object = {
+          prelaunch: !distFlip.prelaunch,
+        };
+        break;
+      case "supply":
+        object = {
+          supply: !distFlip.supply,
+        };
+        break;
+      case "liquidity":
+        object = {
+          liquidity: !distFlip.liquidity,
+        };
+        break;
+      default:
+        break;
+    }
+    setDistFlip((data) => ({ ...data, ...object }));
+  };
+
   return (
     <div className="tokenomics-container" id="tokenomics">
       <div className="tok-title">Tokenomics</div>
@@ -44,7 +102,7 @@ const Tokenomics = () => {
               style={{ width: "60px", height: "60px" }}
               alt="Checkmark"
             />
-            8% Buy/Transfer fees
+            5% Buy/Transfer fees
           </div>
         </Card>
         <Card>
@@ -61,7 +119,7 @@ const Tokenomics = () => {
               style={{ width: "60px", height: "60px" }}
               alt="Checkmark"
             />
-            12% Sell fees
+            5% Sell fees
           </div>
         </Card>
         <Card>
@@ -84,50 +142,110 @@ const Tokenomics = () => {
       </div>
       <div className="tk-card-container">
         <Card>
-          <div className="card-content">
-            <img
-              src={require("./../../assets/images/icons8-capital-96.png")}
-              style={{ width: "180px", height: "120px", margin: "20px" }}
-              alt="Capital"
-            />
-            <span style={{ fontSize: "18px", fontWeight: "bold" }}>
-              7.5% soft cap –<br /> 15% hard cap
-            </span>
+          <div
+            className="card-content"
+            onMouseEnter={flip.bind(this, "prelaunch")}
+            onMouseLeave={flip.bind(this, "prelaunch")}
+          >
+            <div className={"front" + (distFlip.prelaunch ? " flipF" : "")}>
+              <img
+                src={require("./../../assets/images/icons8-capital-96.png")}
+                style={{ width: "180px", height: "120px", margin: "20px" }}
+                alt="Capital"
+              />
+              <span style={{ fontSize: "16px", fontWeight: "bold" }}>
+                Presale / Launch Details
+              </span>
 
-            <span style={{ fontSize: "12px" }}>
-              7,500,000 / 15,000,000 coins
-            </span>
+              <span style={{ fontSize: "12px" }}>Hover/Click for details</span>
+            </div>
+            <div className={"back" + (distFlip.prelaunch ? " unflipB" : "")}>
+              <span style={{ marginBottom: "20px", fontWeight: "bold" }}>
+                Tokens Distribution
+              </span>
+              <span
+                style={{
+                  borderTop: "1px solid #ffffff",
+                  width: "100%",
+                  marginBottom: "20px",
+                }}
+              ></span>
+              {DetailItem("Private sale", ["15,000,000"])}
+              {DetailItem("Listing & Liquidity", ["50,000,000"])}
+            </div>
           </div>
         </Card>
         <Card>
-          <div className="card-content">
-            <img
-              src={require("./../../assets/images/core-team.png")}
-              style={{ width: "180px", height: "120px", margin: "20px" }}
-              alt="Core team"
-            />
-            <span style={{ fontSize: "18px", fontWeight: "bold" }}>
-              18% founders’ tokens
-            </span>
+          <div
+            className="card-content"
+            onMouseEnter={flip.bind(this, "supply")}
+            onMouseLeave={flip.bind(this, "supply")}
+          >
+            <div className={"front" + (distFlip.supply ? " flipF" : "")}>
+              <img
+                src={require("./../../assets/images/core-team.png")}
+                style={{ width: "180px", height: "120px", margin: "20px" }}
+                alt="Core team"
+              />
+              <span style={{ fontSize: "16px", fontWeight: "bold" }}>
+                Supply breakdown
+              </span>
 
-            <span style={{ fontSize: "12px" }}>(Released over 18 months)</span>
+              <span style={{ fontSize: "12px" }}>Hover/Click for details</span>
+            </div>
+            <div className={"back" + (distFlip.supply ? " unflipB" : "")}>
+              <span style={{ marginBottom: "20px", fontWeight: "bold" }}>
+                Tokens Distribution
+              </span>
+              <span
+                style={{
+                  borderTop: "1px solid #ffffff",
+                  width: "100%",
+                  marginBottom: "20px",
+                }}
+              ></span>
+              {DetailItem("Investment Wallet", ["15,000,000"])}
+              {DetailItem("Team & Ops Wallet", ["5,000,000"])}
+              {DetailItem("Marketing Wallet", ["5,000,000"])}
+              {DetailItem("Locked", ["10,000,000"])}
+            </div>
           </div>
         </Card>
         <Card>
-          <div className="card-content">
-            <img
-              src={require("./../../assets/images/liquidity.png")}
-              style={{ width: "180px", height: "120px", margin: "20px" }}
-              alt="Liquidity"
-            />
-            <span style={{ fontSize: "18px", fontWeight: "bold" }}>
-              Liquidity pool
-            </span>
-
-            <span style={{ fontSize: "12px" }}>HZEP / BNB</span>
-            <span style={{ fontSize: "12px" }}>HZEP / BUSD</span>
-            <span style={{ fontSize: "12px" }}>HZEP / POOP</span>
-            <span style={{ fontSize: "12px" }}>HZEP / USDT</span>
+          <div
+            className="card-content"
+            onMouseEnter={flip.bind(this, "liquidity")}
+            onMouseLeave={flip.bind(this, "liquidity")}
+          >
+            <div className={"front" + (distFlip.liquidity ? " flipF" : "")}>
+              <img
+                src={require("./../../assets/images/liquidity.png")}
+                style={{ width: "180px", height: "120px", margin: "20px" }}
+                alt="Liquidity"
+              />
+              <span style={{ fontSize: "16px", fontWeight: "bold" }}>
+                Liquidity pools
+              </span>
+              <span style={{ fontSize: "12px" }}>Hover/Click for details</span>
+            </div>
+            <div className={"back" + (distFlip.liquidity ? " unflipB" : "")}>
+              <span style={{ marginBottom: "20px", fontWeight: "bold" }}>
+                Tokens Distribution
+              </span>
+              <span
+                style={{
+                  borderTop: "1px solid #ffffff",
+                  width: "100%",
+                  marginBottom: "20px",
+                }}
+              ></span>
+              {DetailItem("Initial Liquidity", ["10,000,000"])}
+              {DetailItem("Liquidity Pairs", [
+                "HZEP / BNB",
+                "HZEP / BUSD",
+                "HZEP / USDT",
+              ])}
+            </div>
           </div>
         </Card>
       </div>
